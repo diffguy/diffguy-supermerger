28c3cecf0bc9e44baa7b94482a0ad7523581b214
diffguy <133231939+diffguy@users.noreply.github.com>
2025-01-02 19:57:07 +0000

    Update install.py
    ddiffusers 0.30.2

--- install.py
+++ install.py
@@ -4,4 +4,4 @@ if not launch.is_installed("sklearn"):
     launch.run_pip("install scikit-learn", "scikit-learn")
 
 if not launch.is_installed("diffusers"):
-    launch.run_pip("install diffusers", "diffusers")
+    launch.run_pip("install diffusers==0.30.2", "diffusers")
